cc9b4e2e9c54110aa581fd02b60dffc516a3c024
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 11:56:00 +0100

    Add files via upload

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -1,10 +1,29 @@
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Integer> pozitivet = new ArrayList<>();
+        
+        while (true){
+            int input = scanner.nextInt();
+            if(input == 0){
+                int total = 0;
+                if(pozitivet.isEmpty()){
+                    System.out.println("Cannot calculate the average");
+                } else {
+                    for (int i: pozitivet){
+                        total += i;
+                    }
+                    System.out.println(1.0*total/pozitivet.size());
+                }
+                break;
+            } else if (input > 0){
+                pozitivet.add(input);
+            }
+        }
     }
 }
